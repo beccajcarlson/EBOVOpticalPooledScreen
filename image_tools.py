a919334cdc39ee71a987996f9fc5bd83fefc96a2
stefageo <moartgs@gmail.com>
2022-07-27 09:06:11 -0400

    Initial tooling functions

---
+++ image_tools.py
@@ -0,0 +1,160 @@
+import numpy as np
+import torchvision.transforms.functional as TF
+
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def cell_image(image_tensor, channels=[0, 1, 2, 3, 4, 5],
+               channel_names=["DAPI", "FISH", "VP35",
+                              "Jun", "Vimentin", "LAMP1"],
+               cmap='viridis', norm_0=False, grad_img=False):
+    """Generates a figure representing the selected channels of an input image
+    The figure shows the channels in the order provided in the input list.
+
+    Args:
+        image_tensor (Torch tensor or Numpy array): Image with channels as first axis
+        channels (list, optional): Channels to show. Defaults to [0, 1, 2, 3, 4, 5].
+        channel_names (list, optional): Channel names, in order. Defaults to ["DAPI", "FISH", "VP35", "Jun", "Vimentin", "LAMP1"].
+        cmap (str, optional): Choice of colormap. Defaults to 'viridis'.
+        norm_0 (bool, optional): Normalize colors to have center 0. Defaults to False.
+        grad_img (bool, optional): Image contains gradients per pixel. Defaults to False.
+
+    Returns:
+        Matplotlib figure of channels and names
+    """
+    assert len(channels) == len(
+        channel_names), "Channel and Names lists do not match in size"
+
+    fig, ax = plt.subplots(ncols=len(channels), figsize=(5 * len(channels), 5))
+
+    max_ = image_tensor.max()
+    min_ = image_tensor.min()
+
+    if len(channels) == 1:
+        ax = [ax]
+
+    # Iterate over channels, populating each pane of the image
+    for i, chan in enumerate(channels):
+        if norm_0:
+            # If gradient image, min/max normalize across all channels
+            if grad_img:
+                divnorm = colors.TwoSlopeNorm(vcenter=0, vmin=min_, vmax=max_)
+            else:
+                divnorm = colors.TwoSlopeNorm(vcenter=0)
+
+            im = ax[i].imshow(image_tensor[chan, :, :],
+                              cmap=cmap, norm=divnorm)
+        else:
+            im = ax[i].imshow(image_tensor[chan, :, :], cmap=cmap)
+
+        ax[i].grid(False)
+        ax[i].set_xticks([])
+        ax[i].set_yticks([])
+        ax[i].set_title(channel_names[i], size=20)
+
+        # If gradient image, only show colorbar on final image (same colorbar for all)
+        if (grad_img and (i == len(channels) - 1)) or not grad_img:
+            divider = make_axes_locatable(ax[i])
+            cax = divider.append_axes('right', size='5%', pad=0.05)
+            cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+            cbar.ax.tick_params(labelsize=18)
+
+    plt.show()
+    return fig
+
+
+class Rotate:
+    """ Rotates a cell image and its mask by a random angle 0 - 359 degrees
+
+    Torch custom image transform.
+    """
+
+    def __init__(self, angle=None):
+        """
+        Args:
+            angle (int, optional): Specific, fixed angle to rotate. Defaults to None.
+        """
+        self.angle = angle
+
+    def __call__(self, x, mask):
+        if self.angle is None:
+            angle = np.random.uniform(-180, 180)
+        else:
+            angle = self.angle
+
+        return TF.rotate(x, angle), TF.rotate(mask, angle)
+
+
+class Flip:
+    """ Flip a cell image and its mask over an axis
+
+    Torch custom image transform.
+    """
+
+    def __init__(self, orientation="v", p=1):
+        """
+        Args:
+            orientation (str, optional): Axis to flip, [v]ertical or [h]oriztonal. Defaults to "v".
+            p (int, optional): Probability of flipping the image, versus leaving it as is. Defaults to 1.
+        """
+        assert orientation in [
+            "v", "h"], "Must pick an orientation [v]ertical or [h]oriztonal"
+        self.orientation = orientation
+        self.p = p
+
+    def __call__(self, x, mask):
+        to_rotate = np.random.binomial(1, self.p)
+
+        if to_rotate:
+            return TF.vflip(x) if self.orientation == "v" else TF.hflip(x),\
+                TF.vflip(mask) if self.orientation == "v" else TF.hflip(mask)
+
+        else:
+            return x.clone(), mask.clone()
+
+
+class Gauss:
+    """ Apply Gaussian kernel to a cell image and its mask
+
+    Torch custom image transform.
+    """
+
+    def __init__(self, kernel=5, sigma=(0.2, 1.0)):
+        """
+        Args:
+            kernel (int, optional): Kernel dimension. Defaults to 5.
+            sigma (tuple, optional): Range of variances, chosen over uniformly. Defaults to (0.2, 1.0).
+        """
+        self.kernel = kernel
+        self.sigma = sigma
+
+    def __call__(self, x, mask):
+        sigma_0 = np.random.uniform(*self.sigma)
+
+        # Mask not blurred
+        return TF.gaussian_blur(x, self.kernel, sigma_0), mask.clone()
+
+
+class Affine:
+    """ Apply Affine transform to a cell image and its mask
+
+    Torch custom image transform.
+    """
+
+    def __init__(self, translate=5):
+        """
+        Args:
+            translate (int, optional): Maximum intensity of Affine transform. Defaults to 5.
+        """
+        self.translate = translate
+
+    def __call__(self, x, mask):
+        translate_x = np.random.uniform(-self.translate,
+                                        self.translate)
+        translate_y = np.random.uniform(-self.translate,
+                                        self.translate)
+
+        return TF.affine(x, 0, (translate_x, translate_y), 1, 0),\
+            TF.affine(mask, 0, (translate_x, translate_y), 1, 0)
---
+++ modeling_tools.py
@@ -0,0 +1,138 @@
+import torch
+
+# Width of Regression Bins
+WIDTH = 1/3
+
+# Regression Bin Centers
+CENTERS = [-3/2 * WIDTH, -1/2 * WIDTH, 1/2 * WIDTH, 3/2 * WIDTH]
+
+# Regression Bin Boundaries
+BOUNDARIES = [-WIDTH, 0, WIDTH]
+
+
+def _unused_relabel(label):
+    """ Relabel the initial values provided to numerical 1-4
+
+    Args:
+        label (int): Value 1-4
+
+    Returns:
+        int, None: Relabeled values
+    """
+    if label == 1:
+        return 2
+    elif label == 2:
+        return 4
+    elif label == 3:
+        return 3
+    elif label == 4:
+        return 1
+    elif label is None:
+        return None
+    else:
+        raise AssertionError(f"Shouldn't have label {label}")
+
+
+def approx_eq(a, b):
+    """ Determines if two values are approximately equal
+
+    Within 1e-4 tolerance on either side
+
+    Args:
+        a (float): value 1
+        b (float): value 2
+
+    Returns:
+        bool: a == b within 1e-4 boundary
+    """
+    return b - 1e-4 <= a <= b + 1e-4
+
+
+def relabel_for_regression(label):
+    return CENTERS[int(label) - 1]
+
+
+def relabel_for_regression_optimized(labels, label_max=4, label_min=1):
+    """ Relabels a set of labels in to the values prescribed by CENTERS
+
+    Args:
+        labels (array): Labels to be reformatted
+        label_max (int, optional): Maximum Label. Defaults to 4.
+        label_min (int, optional): Minimum label. Defaults to 1.
+
+    Returns:
+        array: Relabeled array of labels
+    """
+    norm_factor = (CENTERS[-1] - CENTERS[0]) / (label_max - label_min)
+    labels *= norm_factor
+    labels -= label_min * norm_factor - CENTERS[0]
+    return labels
+
+
+def regression_correctness(truth, pred):
+    """ Determines the correctness of a regression prediction on a single sample
+
+    Correct is defined as the regression prediction falling into the bucket given by
+    the ground truth.
+
+    Args:
+        truth (float): Ground truth (in CENTERS)
+        pred (float): Regression prediction
+
+    Returns:
+        bool: True if the regression prediction was in the correct bucket
+    """
+    if pred <= BOUNDARIES[0]:
+        return approx_eq(truth, CENTERS[0])
+    elif pred >= BOUNDARIES[2]:
+        return approx_eq(truth, CENTERS[3])
+    elif BOUNDARIES[1] <= pred < BOUNDARIES[2]:
+        return approx_eq(truth, CENTERS[2])
+    elif BOUNDARIES[0] < pred < BOUNDARIES[1]:
+        return approx_eq(truth, CENTERS[1])
+
+
+def regression_label_preds(pred):
+    """ Takes a regression prediction and buckets it based on its value
+
+    Potential values are 1, 2, 3, 4 based on prediction position relative
+    to BOUNDARIES constant.
+
+    Args:
+        pred (float): Regression prediction
+
+    Returns:
+        int: Bucketed prediction in [1, 2, 3, 4]
+    """
+    if pred <= BOUNDARIES[0]:
+        return 1
+    elif pred >= BOUNDARIES[2]:
+        return 4
+    elif BOUNDARIES[1] <= pred < BOUNDARIES[2]:
+        return 3
+    elif BOUNDARIES[0] < pred < BOUNDARIES[1]:
+        return 2
+
+
+def humanize_pred(pred):
+    """ Take a regression prediction and make it readable
+
+    Takes regression predictions and rescales them such that
+    the CENTERS become 1, 2, 3, 4
+
+    Args:
+        pred (array or float): Regression prediction
+
+    Returns:
+        array or float: Rescaled predictions
+    """
+    return (pred + 5/2 * WIDTH) / WIDTH
+
+
+def get_device():
+    """ Get torch device for model training 
+
+    Returns:
+        str: torch device descriptor
+    """
+    return 'cuda:0' if torch.cuda.is_available() else 'cpu'
